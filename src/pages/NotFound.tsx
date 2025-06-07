2e9afa06ed3b507696f5c8610022bb2a0ab09563
kongduk <gongduk4960@naver.com>
2025-06-08 00:09:30 +0900

    Design(#2):NotFound css변경

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -7,8 +7,6 @@ function NotFound() {
         // 다운이미지 사용
         <div className={styles.container}>
             <img src="/NotFound.svg" alt="에러페이지" />
-            <br />
-            <br />
             {/* 메인페이지 이동으로 변경 */}
             <Link to="/" className={styles.gotomain}>&lt;&nbsp; 메인 페이지로 이동하기</Link>
         </div>
