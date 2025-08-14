2fc34f7682be5288eb2c0ecc4cfbe984d8286b69
kongduk <gongduk4960@gmail.com>
2025-08-14 21:31:25 +0900

    loginbutton 코드 롤백

--- src/pages/Teacher/AddTimeLine/index.tsx
+++ src/pages/Teacher/AddTimeLine/index.tsx
@@ -2,9 +2,34 @@ import TimeLine from "@/features/Teacher/AddTimeLine";
 import { IoInformationCircleOutline } from "react-icons/io5";
 import { MdOutlineFileDownload } from "react-icons/md";
 import * as s from './styles';
+import { useRef } from "react";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 
 
 export default function AddTimeLine() {
+  const timeLineRef = useRef<HTMLDivElement>(null);
+
+  const handleSavePDF = async () => {
+    if (!timeLineRef.current) return;
+    const element = timeLineRef.current;
+    const canvas = await html2canvas(element, { scale: 2 });
+    const img = canvas.toDataURL("image/png");
+    const pdf = new jsPDF({ orientation: "landscape", unit: "pt", format: "a4" });
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = pdf.internal.pageSize.getHeight();
+    // 화면에 맞게 넓이 구하기
+    const imgExtent = canvas.width / canvas.height;
+    let width = pdfWidth;
+    let height = pdfWidth / imgExtent;
+    if (height > pdfHeight) {
+      height = pdfHeight;
+      width = pdfHeight * imgExtent;
+    }
+    pdf.addImage(img, "PNG", (pdfWidth - width) / 2, 40, width, height - 40);
+    pdf.save("시간표.pdf");
+  };
+
   return (
     <s.Container>
       <s.Title>시간표 수정하기</s.Title>
@@ -27,13 +52,16 @@ export default function AddTimeLine() {
             <s.GoogleSheetB>
               구글 시트에서 가져오기
             </s.GoogleSheetB>
-            <s.PDFB>
-            <MdOutlineFileDownload /> PDF로 저장
+            <s.PDFB onClick={handleSavePDF}>
+              <MdOutlineFileDownload /> PDF로 저장
             </s.PDFB>
           </s.ButtonGroup>
         </s.LeftPanel>
+
         <s.RightPanel>
-          <TimeLine />
+          <div ref={timeLineRef}>
+            <TimeLine />
+          </div>
         </s.RightPanel>
       </s.ContentWrapper>
     </s.Container>
