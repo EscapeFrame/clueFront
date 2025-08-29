4dd6f72806a974cabda62101e5d852f4322f95bb
kongduk <gongduk4960@gmail.com>
2025-08-30 02:02:07 +0900

    feat(#124): 로그인 페이지 제작 완료

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -3,13 +3,35 @@ import { theme } from '@/shared/theme/theme.styles';
 import { fonts } from '@/shared/theme/font.styles';
 
 export const Container = styled.div`
-justify-content: center;
-position: relative;
-img {
-    width: 50%;
-    height: 50%;
-    object-fit: cover;
-    display: block;
-  }
+  justify-content: center;
+  position: relative;
+  margin-top: 10%;
+  img {
+        width: 20%;
+        height: 20%;
+        object-fit: cover;
+        display: block;
+      }
+`;
+
+export const LogoBox = styled.div`
+  justify-content: center;
+  margin:auto;
+  display: flex;
+  text-align: center;
+`
+
+export const LoginScript = styled.div `
+  text-align: center;
+  margin-top : 20px;
+  margin-bottom : 60px;
+  ${fonts.P3}
+`
+
+export const AgreementScript = styled.div `
+  text-align: center;
+  margin-top : 10px;
+  ${fonts.P2}
+  color: ${theme.colors.gray[600]}
 `
 
