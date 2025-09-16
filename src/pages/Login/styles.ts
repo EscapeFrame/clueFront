568339c7ef6cb3baeaf70dd97ca25ef7e5ba6e6b
kongduk <gongduk4960@naver.com>
2025-09-16 16:30:08 +0900

    Design(#194):로고 css 수정

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -3,7 +3,6 @@ import { theme } from '@/shared/theme/theme.styles';
 import { fonts } from '@/shared/theme/font.styles';
 
 export const Container = styled.div`
-  display: flex;
   justify-content: center;
   position: relative;
   margin-top: 10%;
@@ -17,26 +16,18 @@ export const Container = styled.div`
 
   @media (max-width: 1200px) {
     img {
-      width: 25%;
-      height: 25%;
+      width: 20%;
+      height: 20%;
     }
   }
 
   @media (max-width: 768px) {
-    margin-top: 8%;
     img {
       width: 40%;
       height: 40%;
     }
   }
 
-  @media (max-width: 480px) {
-    margin-top: 6%;
-    img {
-      width: 60%;
-      height: 60%;
-    }
-  }
 `;
 
 export const LogoBox = styled.div`
@@ -44,9 +35,10 @@ export const LogoBox = styled.div`
   margin: auto;
   display: flex;
   text-align: center;
+  height: 100%;
+  width: 100%;
 
   @media (max-width: 768px) {
-    flex-direction: column;
     gap: 8px;
   }
 `;
