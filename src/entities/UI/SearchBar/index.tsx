f13d798550791da566303113bdb89910629c49a6
kongduk <gongduk4960@naver.com>
2025-09-17 23:11:30 +0900

    Refactor(#194): 검색어 필터기능 onchange발생마다 조회하도록 리팩토링

--- src/entities/UI/SearchBar/index.tsx
+++ src/entities/UI/SearchBar/index.tsx
@@ -8,10 +8,21 @@ type SearchProps = {
 export function SearchBar({ onSearch }: SearchProps) {
   const [searchQuery, setSearchQuery] = useState('');
 
-  const handleSearch = () => onSearch(searchQuery);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+    onSearch(query);
+    console.log("검색어:", query);
+  };
+
+  const handleSearch = () => {
+    onSearch(searchQuery);
+  };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') handleSearch();
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
   };
 
   return (
@@ -20,7 +31,7 @@ export function SearchBar({ onSearch }: SearchProps) {
         type="text"
         placeholder="찾으시는 학습실을 입력해주세요."
         value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
+        onChange={handleInputChange}
         onKeyDown={handleKeyDown}
       />
       <s.SearchIcon size={20} onClick={handleSearch} />
