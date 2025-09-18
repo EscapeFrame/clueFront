be08c08bb7ce73ce273217a9de52594813da4259
kongduk <gongduk4960@naver.com>
2025-09-18 20:18:46 +0900

    refactor(#205): 토큰삭제 코드 제거

--- src/app/hooks/useAccessToken.ts
+++ src/app/hooks/useAccessToken.ts
@@ -4,30 +4,26 @@ import { userState } from '@/shared/model/userState';
 import { useRecoilState } from 'recoil';
 
 export const useAuth = () => {
-  const [accessToken, setAccessToken] = useState<string | null>(() => {
-    return localStorage.getItem('accessToken');
-  });
-  const [refreshToken, setRefreshToken] = useState<string | null>(() => {
-    return localStorage.getItem('refreshToken');
-  });
+  const [accessToken, setAccessToken] = useState<string | null>(localStorage.getItem('accessToken'));
+  const [refreshToken, setRefreshToken] = useState<string | null>(localStorage.getItem('refreshToken'));
 
   const [user, setUser] = useRecoilState(userState);
 
   // 로그인시 사용자 정보 및 토큰 세팅
   const setAuthInfo = useCallback((accessToken: string, refreshToken: string) => {
-    localStorage.setItem('accessToken', accessToken);
-    localStorage.setItem('refreshToken', refreshToken);
     setAccessToken(accessToken);
     setRefreshToken(refreshToken);
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('refreshToken', refreshToken);
   }, []);
 
   // 로그아웃
   const removeAuthInfo = useCallback(() => {
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('refreshToken');
     setAccessToken(null);
     setRefreshToken(null);
     setUser({ username: "", userId: "", role: "" });
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
   }, [setUser]);
   
   // 토큰은 있으나 유저 정보가 없을 경우
@@ -51,7 +47,6 @@ export const useAuth = () => {
         });
       } catch (error) {
         console.error('유저 정보 조회 실패: ', error);
-        removeAuthInfo();
       }
     };
     fetchUserInfo();
