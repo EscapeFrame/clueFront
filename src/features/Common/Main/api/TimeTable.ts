d26ffa5b9cde0ee19b8f9a417de65a350f2377c6
kongduk <gongduk4960@naver.com>
2025-09-26 08:23:31 +0900

    Refactor(#280): 시간표 api Url 필요없는 변수 제거

--- src/features/Common/Main/api/TimeTable.ts
+++ src/features/Common/Main/api/TimeTable.ts
@@ -1,11 +1,10 @@
 import CustomApi from "@/shared/config/api";
-import { ScheduleItem } from '@/shared/types/schedule';
 
 // <-- timeline api -->
 export const timetableApi = {
-  getWeeklyTimetable: async (grade: number, classNumber: number): Promise<ScheduleItem[] | number> => {
+  getWeeklyTimetable: async ()=> {
     try {
-      const res = await CustomApi.get(`/api/timetable/weekly?grade=${grade}&classNumber=${classNumber}`);
+      const res = await CustomApi.get(`/api/timetable/weekly`);
       if (res.status !== 200) return res.status;
       return res.data;
     } catch (error) {
