eadba82e11d51fca44dbf293b6d27a1be1325960
kongduk <gongduk4960@naver.com>
2025-10-22 19:52:55 +0900

    Design(#297): 학생 메인페이지 디자인 변경

--- src/pages/Student/Main/index.tsx
+++ src/pages/Student/Main/index.tsx
@@ -17,12 +17,15 @@ const HomeLayout: React.FC<HomeLayoutProps> = ({ children }) => {
 export default function HomePage() {
   return (
     <HomeLayout>
-      <MySchedule />
-      <PendingTask />     {/* 미제출과제*/}
-      <TaskGuide />       {/* 수행평가 안내*/}
-      <Notice />          {/* 공지/안내 */}
-      {/* 성취도 분석(추후 추가예정) */}
-      <QuickLink />       {/* 학교서비스바로가기 */}
+      <s.Left>
+        <MySchedule />
+        <PendingTask />     {/* 미제출과제*/}
+        <TaskGuide />       {/* 수행평가 안내*/}
+        <QuickLink />       {/* 학교서비스바로가기 */}
+      </s.Left>
+      <s.Right>
+        <Notice />          {/* 공지사항 */}
+      </s.Right>
     </HomeLayout>
   );
 }
