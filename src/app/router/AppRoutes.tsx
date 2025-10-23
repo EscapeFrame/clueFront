dbef892ffd7b139cd61a59f2443a3026f687682c
kongduk <gongduk4960@naver.com>
2025-10-23 10:11:11 +0900

    Design(#302): register페이지 version2 퍼블리싱

--- src/app/router/AppRoutes.tsx
+++ src/app/router/AppRoutes.tsx
@@ -74,6 +74,7 @@ export const AppRoutes = ({ role }: AppRoutesProps) => {
 
       {isAuthenticated && (
         <>
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/setting" element={<Setting />} />
           <Route path="/linksave/*" element={<LinkSaveRoutes />} />
         </>
