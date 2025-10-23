36b0c100876ae6e2f050fe1d6a20db7e0cf2bfa9
kongduk <gongduk4960@naver.com>
2025-10-23 16:58:17 +0900

    Design(#302): 에러페이지 리디자인

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -4,15 +4,25 @@ import { useLocation, useNavigate } from 'react-router-dom';
 export default function NotFound() {
   const navigate = useNavigate();
   const location = useLocation();
-  const message = location.state?.message || '페이지를 찾을 수 없습니다.';
+  const errorCode = location.state?.errorCode || 404;
+  const customMessage = location.state?.message;
 
   return (
     <s.Container>
+      <div>
+        <s.ErrorCode>{errorCode} <br /> <b>Error</b></s.ErrorCode>
+        <s.Bar></s.Bar>
+        <s.Message>죄송합니다. <br />{customMessage || '페이지를 찾을 수 없습니다.'}</s.Message>
+        <s.ButtonContainer>
+          <s.GoToMain onClick={() => navigate(-1)}>
+            이전으로
+          </s.GoToMain>
+          <s.Retry onClick={() => window.location.reload()}>
+            재시도
+          </s.Retry>
+        </s.ButtonContainer>
+      </div>
       <s.Image src="/NotFound.png" alt="error" />
-      <s.Message>{message}</s.Message>
-      <s.GoToMain onClick={() => navigate(-1)}>
-        &lt;&nbsp; 이전 페이지로 돌아가기
-      </s.GoToMain>
     </s.Container>
   );
 }
