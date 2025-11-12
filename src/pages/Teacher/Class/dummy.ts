60359b6cb8a0d29ee01a787f4fd41edd20e1223a
Rohyoohyun <unternehmen02191@gmail.com>
2025-11-13 02:05:52 +0900

    fix(#361): Exam css 내용 수정

--- src/pages/Teacher/Class/dummy.ts
+++ src/pages/Teacher/Class/dummy.ts
@@ -1,3 +1,4 @@
+import { ExamDetail } from '@/shared/types/Class/Exam';
 import { Directory, NewsItem, QuestionItem } from '@/shared/types/Class/Lesson';
 
 export const dummyDirectories: Directory[] = [
@@ -25,3 +26,35 @@ export const dummyDirectories: Directory[] = [
 ];
 
 export const dummyClassCode = "12345";
+
+export const dummyExams: ExamDetail[] = [
+  {
+    id: 'exam1',
+    title: '중간고사',
+    date: '2025-11-12',
+    status: 0,
+    description: '중간고사 범위: 1~5장',
+    files: [
+      {
+          id: 'file1',
+          fileName: '문서1.pdf',
+          fileSize: undefined,
+          name: ''
+      },
+      {
+          id: 'file2',
+          fileName: '문서2.pdf',
+          fileSize: undefined,
+          name: ''
+      },
+    ],
+  },
+  {
+    id: 'exam2',
+    title: '기말고사',
+    date: '2025-12-20',
+    status: 1,
+    description: '기말고사 범위: 전체',
+    files: [],
+  },
+];
