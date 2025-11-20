46f5d79b9bf6a8f833b75c09eb10357811bb3015
kongduk <gongduk4960@naver.com>
2025-11-20 19:10:14 +0900

    Fix(#434): 선생님 수업실 페이지 디자인 수정

--- src/pages/Teacher/Class/styles.ts
+++ src/pages/Teacher/Class/styles.ts
@@ -2,7 +2,6 @@ import { colors } from '@/shared/theme/theme.styles';
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  background-color: ${colors.gray[1]};
   height: 125vh;
 `;
 
